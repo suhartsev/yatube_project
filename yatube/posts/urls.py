ab493e6f9b516d239e5569182b3d55568f6d3c24
Sergey Sukhartsev <bloxazavr@mail.ru>
2022-12-13 12:34:45 +0300

    icon

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug>/', views.group_posts),
+    path('', views.index, name='index'),
+    path('group_list.html', views.group_posts, name='group_posts'),
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    template = 'posts/index.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
 
-def group_posts(request, slug):
-    return HttpResponse(f'Группа {slug}')     
+
+def group_posts(request):
+    template = 'posts/group_list.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)     
 
 # Create your views here.

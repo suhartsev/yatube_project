8005b3c2a53fa4ce8307e2babc677c6df708ebe8
Sergey Sukhartsev <bloxazavr@mail.ru>
2022-12-13 22:24:43 +0300

    title

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,8 +5,11 @@ from .models import Post, Group
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     template = 'posts/index.html'
+    title = 'Последние обновления на сайте'
     context = {
         'posts': posts,
+        'title': title,
+        
     }
     return render(request, template, context)
 
@@ -15,8 +18,10 @@ def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = "Записи сообщества 'Группа тайных поклонников графа'"
     context = {
         'group': group,
         'posts': posts,
+        'title': title,
     }
     return render(request, template, context)
